d528d806f7d8b7b882f08beccff1fc100a7707f3
Admin <Admin@DESKTOP-26E3KH4>
2023-10-13 22:36:24 +0530

    [sujata]

---
+++ src/packages/stud.java
@@ -0,0 +1,23 @@
+package packages;
+
+public class stud {
+	public static void main(String[] args)
+	{
+		String studmname;
+		int studno;
+		char studgrade;
+		float studper;
+		
+		 studmname="sujata";
+		studno= 10;
+		studgrade= 'A';
+		studper= 80f;
+		
+
+		System.out.println("stud name:   "+studmname);
+		System.out.println("stud no:   "+studno);
+		System.out.println("stud grade:   "+studgrade);
+		System.out.println("stud per:   "+studper +"%");
+	}
+
+}
